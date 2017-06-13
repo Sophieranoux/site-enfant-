dc73cb04de8431541b47a46a9a4edc2c20fae72d
Pi-Lou <pierrelouis.cohen@gmail.com>
2017-06-13 18:48:36 +0200

    suppresion our-team et .html sur team

--- apps.js
+++ apps.js
@@ -3,6 +3,18 @@ var app = express();
 
 app.use(express.static("static"));
 
+app.get("/team", function(req, res){
+    res.sendFile('/static/team.html',{
+            root: __dirname ,
+            dotfiles: 'deny',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        }
+    );
+});
+
 app.listen("3129", function(err){
     console.log("Application ready listening on port 3129");
 });
