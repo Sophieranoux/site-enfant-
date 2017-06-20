c172598c228f9807e865a3661f3775e3d887d8fa
Sophieranoux <sophie@MacBook-Pro-de-Sophie.local>
2017-06-20 19:16:36 +0200

    Master cricri

--- apps.js
+++ apps.js
@@ -1,30 +1,41 @@
 var express = require("express");
 var app = express();
-
-app.use(express.static("static"));
-
-app.post("/action", function(req,res){
-/** express bodyparser */
-});
-
-app.get("/team", function(req, res){
-    res.sendFile('/static/team.html',{
-            root: __dirname ,
-            dotfiles: 'deny',
-            headers: {
-                'x-timestamp': Date.now(),
-                'x-sent': true
-            }
-        }
-    );
-});
-
-app.listen("3129", function(err){
-    console.log("Application ready listening on port 3129");
-});
-
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://essec:cergyisc00l@138.68.110.210:27017/admin?readPreference=primary';
 
-app.use(bodyParser.json());
-app.use(express.static("static"));
+MongoClient.connect(url, function(err, db) {
+
+    var collection = db.collection("contactsiteenfant");
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(express.static("static"));
+
+    app.post("/action", function(req,res){
+        collection.insert(req.body);
+        res.redirect("/team");
+    });
+
+    app.get("/list",function(req, res){
+        collection.find().toArray(function(err,results){
+            res.send(results);
+        });
+    });
+
+    app.get("/team", function(req, res){
+        res.sendFile('/static/team.html',{
+                root: __dirname ,
+                dotfiles: 'deny',
+                headers: {
+                    'x-timestamp': Date.now(),
+                    'x-sent': true
+                }
+            }
+        );
+    });
+
+    app.listen("3129", function(err){
+        console.log("Application ready listening on port 3129");
+    });
+});
